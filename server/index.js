ae4de74189cc9027b449cb3fdb00ec88daab65c3
Muhammad-Saad12 <muhammadsaad7250@gmail.com>
2023-05-14 03:18:42 +0500

    Cleaned up messy code

--- server/index.js
+++ server/index.js
@@ -1,35 +1,27 @@
 const mongoose = require("mongoose");
-const session=require('express-session');
-const passport = require('passport');
-require('./auth')
+const session = require("express-session");
+const passport = require("passport");
+require("./auth");
 require("dotenv").config();
-const express = require('express')
-const app = express()
+const express = require("express");
+const app = express();
 
-app.use(session(
-  {
-    secret:process.env.SESSION_SECRET,
-}
-  
-  ))
-app.use(passport.initialize())
-app.use(passport.session())
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+  })
+);
+app.use(passport.initialize());
+app.use(passport.session());
 
+const cors = require("cors");
 
-
-
-const cors = require('cors');
-
-const port = process.env.PORT || 5000
-app.use(express.json())
+const port = process.env.PORT || 5000;
+app.use(express.json());
+const router=require('./routes/admin');
 
 
 app.use(cors());
-
-
-
-
-
 
 mongoose.connect(process.env.MONGODB_URI, {
   useNewUrlParser: true,
@@ -48,78 +40,12 @@ mongoose.connection.on("disconnected", () => {
   console.log("Disconnected from MongoDB Atlas");
 });
 
-app.use(express.urlencoded({extended:false}))
-
-
-function isLoggedIn(req, res, next) {
-  if (req.user && req.user.emails && req.user.emails[0].value === process.env.AUTHORIZE_EMAIL) {
-    return next();
-  } else {
-    return res.sendStatus(401);
-  }
-}
+app.use(express.urlencoded({ extended: false }));
 
 
 
+app.use(router);
 
-
-app.get('/', (req, res) => {
-  res.send('<a href="/admin-panel">Authenticate with Google</a>');
-})
-
-app.get('/admin-portal', isLoggedIn, (req, res) => {
-
-    res.send(`Hello ${req.user.displayName}`);
- 
+app.listen(port, () => {
+  console.log(`App listening on port ${port}`);
 });
-
-
-
-app.get('/logout', (req, res) => {
-  req.logout((err) => {
-    if (err) {
-      console.error(err);
-      return next(err);
-    }
-    req.session.destroy();
-    res.send('Goodbye!');
-  });
-});
-
-
-
-
-app.get('/admin-panel',
-    passport.authenticate('google',{
-      scope:['email','profile'],
-      
-    })
-)
-
-app.get('/auth/admin',
-passport.authenticate('google',{
-  failureRedirect:'/failed',
-  successRedirect:'/admin-portal'
-}),
-
-)
-
-app.get('/failed',(req,res)=>{
-
-  try{
-    req.session.destroy();
-  }catch(err){
-    console.log(err)
-  }
-
-  res.send('You failed to login')
-  
- 
-})
-
-
-
-
-app.listen(port, ()=>{ console.log(`App listening on port ${port}`)
-
-})
